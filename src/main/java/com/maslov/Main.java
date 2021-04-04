741810addadc772941fb7f29562faef9933666ca
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2021-04-04 18:26:29 +0500

    handlers

--- src/main/java/com/maslov/Main.java
+++ src/main/java/com/maslov/Main.java
@@ -1,7 +1,40 @@
 package com.maslov;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
+
+        server.addHandler("GET", "/index.html", Main::sendResponse);
+        server.addHandler("GET", "/spring.svg", Main::sendResponse);
+        server.addHandler("GET", "/spring.png", Main::sendResponse);
+//        server.addHandler("POST", "/messages", (request, responseStream) -> {
+//            sendResponse(request, responseStream);
+//        });
+
+        server.listen(9999);
+    }
+
+    public static void sendResponse(Request request, BufferedOutputStream responseStream) {
+        final var filePath = Path.of("src", "public", request.getPath());
+        final String mimeType;
+        try {
+            mimeType = Files.probeContentType(filePath);
+            final var length = Files.size(filePath);
+            responseStream.write(("HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: " + mimeType + "\r\n" +
+                    "Content-Length: " + length + "\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n"
+            ).getBytes());
+            Files.copy(filePath, responseStream);
+            responseStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/maslov/MyThread.java
+++ src/main/java/com/maslov/MyThread.java
@@ -1,33 +1,58 @@
 package com.maslov;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.Thread.currentThread;
 
 public class MyThread implements Runnable {
-    private final Socket accept;
+    private final Socket socket;
     private final List<String> validPath;
-    private final BufferedReader in;
-    private final BufferedWriter out;
+    private final Map<String, Map<String, Handler>> handlers;
 
-    public MyThread(List<String> validPath, Socket accept) throws IOException {
-        this.accept = accept;
+    public MyThread(List<String> validPath, Socket socket, Map<String, Map<String, Handler>> handlers) throws IOException {
+        this.socket = socket;
         this.validPath = validPath;
-        in = new BufferedReader(new InputStreamReader(accept.getInputStream(), StandardCharsets.UTF_8));
-        out = new BufferedWriter(new OutputStreamWriter(accept.getOutputStream(), StandardCharsets.UTF_8));
+        this.handlers = handlers;
     }
 
     @Override
     public void run() {
-        try {
+        try (final InputStream in = socket.getInputStream();
+             final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
             while (!currentThread().isInterrupted()) {
-                String requestLine = in.readLine();
+                Request requestLine = Request.fromInputStream(in);
+                Map<String, Handler> handlerMap = handlers.get(requestLine.getMethod());
+                if (handlerMap != null) {
+                    Handler handler = handlerMap.get(requestLine.getPath());
+                    if (handler != null) {
+                        handler.handle(requestLine, out);
+                    } else {
+                        invalidPath(out);
+                    }
+                } else {
+                    invalidPath(out);
+                }
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void invalidPath(BufferedOutputStream out) {
+        try {
+            out.write(("HTTP/1.1 404Not Found\r\n" +
+                    "Content-Length: 0\r\n" +
+                    "Connection: close\r\n" +
+                    "\r\n"
+            ).getBytes());
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
